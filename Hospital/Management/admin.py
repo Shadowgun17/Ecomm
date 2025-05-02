eb24b9bc5c75266b420b8259bb9ef94c306f8c28
Shadowgun17 <shadowgun161718@gmail.com>
2025-05-02 18:46:34 +0530

    Update

--- Hospital/Management/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Management.models.user import User
-from Management.models.patient import Patient
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Patient)
